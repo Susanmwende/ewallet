19db61c1560a14d8cb98af5378cf633620ddc39b
Susanmwende <mwendezmasaku@gmail.com>
2024-07-08 08:55:47 +0300

    First Commit

---
+++ app/src/main/java/com/akirachix/ewallet/MyWallet.kt
@@ -0,0 +1,8 @@
+package com.akirachix.ewallet
+
+data class MyWallet(
+    var balance : String,
+    var transaction : String,
+    var moneyin:String,
+    var moneyout : String
+)
---
+++ app/src/main/java/com/akirachix/ewallet/WalletAdapter.kt
@@ -0,0 +1,21 @@
+package com.akirachix.ewallet
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class WalletAdapter (var wallet:List<MyWallet>):
+RecyclerView.Adapter<WalletViewsHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WalletViewsHolder {
+        val itemView = layout
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: WalletViewsHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
